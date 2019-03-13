562aa7573f6630f74fea5e07b14a3b94bf3798ce
Kyle Smith <k.kyle.smith13@gmail.com>
2019-03-13 18:55:54 -0500

    added production gems and initialized

---
+++ db/seeds.rb
@@ -0,0 +1,15 @@
+Team.destroy_all
+User.destroy_all
+
+3.times do
+  team = Team.create(
+    name: Faker::Team.name,
+    sport: Faker::Team.sport
+  )
+
+  5.times do
+    team.users.create(
+      name: Faker::Name.unique.name
+    )
+  end
+end
